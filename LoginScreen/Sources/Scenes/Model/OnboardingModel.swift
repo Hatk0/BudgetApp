2edd5dd23b233ef76b8e8175d196058bc14c8bfc
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-14 12:02:44 +0300

    Add image and in the OnboardingModel file create a class, which define a method with array creation

--- LoginScreen/Sources/Scenes/Model/OnboardingModel.swift
+++ LoginScreen/Sources/Scenes/Model/OnboardingModel.swift
@@ -1,8 +1,14 @@
-//
-//  OnboardingModel.swift
-//  LoginScreen
-//
-//  Created by Dmitry Yastrebov on 14.10.2023.
-//
-
 import Foundation
+
+final class OnboardingModel {
+    
+    func createModels() -> [Onboarding] {
+        return [
+            Onboarding(index: 0,
+                       title: "Retro occupy",
+                       description: "Being the savage's bowsman, that is, the person who pulled the bow-oar in.",
+                       imageName: "man"
+                      )
+        ]
+    }
+}
