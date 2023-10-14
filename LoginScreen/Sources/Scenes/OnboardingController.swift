4540cd4ce57a791f62e35c50db1a02c9af67a706
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-14 11:54:25 +0300

    Convert parent view to OnboardingView and assign the value of View to the created OnboardingView() class

--- LoginScreen/Sources/Scenes/OnboardingController.swift
+++ LoginScreen/Sources/Scenes/OnboardingController.swift
@@ -1,8 +1,15 @@
 import UIKit
 
-class OnboardingController: UIViewController {
+final class OnboardingController: UIViewController {
+    
+    private var onboardingView: OnboardingView? {
+        guard isViewLoaded else { return nil }
+        return view as? OnboardingView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view = OnboardingView()
     }
 }
