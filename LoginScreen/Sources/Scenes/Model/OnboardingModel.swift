314d393ec1b77689c2e9b413b74a084450ae6db1
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-14 11:51:12 +0300

    Set up a project to work with MVC

---
+++ LoginScreen/Sources/Scenes/Model/OnboardingModel.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingModel.swift
+//  LoginScreen
+//
+//  Created by Dmitry Yastrebov on 14.10.2023.
+//
+
+import Foundation
---
+++ LoginScreen/Sources/Scenes/OnboardingController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class OnboardingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
