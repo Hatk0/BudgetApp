febe2353ba497c06ac0f4810aab81e36519371f6
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-28 17:52:30 +0300

    Create settingImageView and make constraints

--- LoginScreen/Sources/Scenes/View/SettingsView/Cell/SettingsTableViewCell.swift
+++ LoginScreen/Sources/Scenes/View/SettingsView/Cell/SettingsTableViewCell.swift
@@ -7,6 +7,12 @@ class SettingsTableViewCell: UITableViewCell {
 
     // MARK: - UI
     
+    private lazy var settingImageView: UIImageView = {
+        let image = UIImageView()
+        image.tintColor = UIColor.label
+        return image
+    }()
+    
     // MARK: - Initializers
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -22,11 +28,16 @@ class SettingsTableViewCell: UITableViewCell {
     // MARK: - Setup
     
     private func setupHierarchy() {
-        
+        addSubview(settingImageView)
     }
     
     private func setupLayout() {
-        
+        settingImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+            make.width.equalTo(50)
+            make.height.equalTo(50)
+        }
     }
     
     // MARK: - Private methods
