58b4c32f3960046ddafe7560e3aac2835f08de57
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-28 17:20:30 +0300

    Create pinkView and make constraints

--- LoginScreen/Sources/Scenes/View/SettingsView/BarView.swift
+++ LoginScreen/Sources/Scenes/View/SettingsView/BarView.swift
@@ -5,6 +5,17 @@ final class BarView: UIView {
     
     // MARK: - UI
     
+    private lazy var pinkView: UIView = {
+       let view = UIView()
+        view.backgroundColor = UIColor(red: 1, green: 0.2, blue: 0.47, alpha: 1)
+        view.layer.shadowColor = UIColor(red: 1, green: 0.2, blue: 0.471, alpha: 0.2).cgColor
+        view.layer.shadowOpacity = 1
+        view.layer.shadowOffset = .zero
+        view.layer.shadowRadius = 42
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    
     // MARK: - Initializers
     
     init() {
@@ -25,10 +36,15 @@ final class BarView: UIView {
     }
     
     private func setupHierarchy() {
-    
+        addSubview(pinkView)
     }
     
     private func setupLayout() {
-        
+        pinkView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(100)
+        }
     }
 }
